9e0f812ba6413eac858258f472be9c7b3a763c4b
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 23:25:55 -0400

    changed logic to show user category value

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
   has_many :accounts
   has_many :users, through: :accounts
+
+  def user_category_value_calc(user)
+    user_category_value = 0
+    self.accounts.each do |account|
+      if account.user_id == user.id
+        user_category_value = user_category_value + account.dollar_value
+      end
+    end
+    user_category_value
+  end
 end
