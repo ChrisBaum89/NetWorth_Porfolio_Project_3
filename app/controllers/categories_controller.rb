316c31872c5e13ee8be6adbebfebc8a90d00d512
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 21:30:50 -0400

    create methods for initial routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,26 @@
 class CategoriesController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
 end
