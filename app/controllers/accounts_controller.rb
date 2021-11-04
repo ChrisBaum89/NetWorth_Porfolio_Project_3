7657c62559aa2d77c472980227d9b740a86a735d
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 21:29:23 -0400

    added initial methods for routes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,25 @@
 class AccountsController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
 end
