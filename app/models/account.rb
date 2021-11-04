b3622e3c354fb9a72db8d8092cee7a9e62d6e9e2
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 23:34:39 -0400

    updated asset_or_debt logic to keep a negative number negative

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@ class Account < ApplicationRecord
   belongs_to :category
 
   def asset_or_debt
-    if self.account_type == "debt"
+    if self.account_type == "debt" && self.dollar_value > 0
       self.dollar_value = 0 - self.dollar_value
     end
     self.save
