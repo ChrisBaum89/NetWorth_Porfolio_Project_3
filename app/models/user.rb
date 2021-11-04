aecaf05eedc2f91d4bc2159d2f8ddfd6d2dd45ed
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 23:09:32 -0400

    added net_worth_calc logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
 
   has_many :accounts
   has_many :categories, through: :accounts
+
+  def net_worth_calc
+    self.net_worth = 0
+    self.accounts.each do |account|
+      if account.account_type == "asset"
+        self.net_worth = self.net_worth + account.dollar_value
+      elsif account.account_type == "debt"
+        self.net_worth = self.net_worth - account.dollar_value
+      else
+      end
+    end
+    self.save
+    self.net_worth
+  end
 end
