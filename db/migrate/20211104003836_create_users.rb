50ca96be7f4b224a57d5d8fed9c9c2cfe7424465
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 20:41:09 -0400

    added columns

--- db/migrate/20211104003836_create_users.rb
+++ db/migrate/20211104003836_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.integer :net_worth
       t.timestamps
     end
   end
