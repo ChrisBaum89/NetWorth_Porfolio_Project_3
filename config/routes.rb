723236c353dbd1f0e552d17f596b4d5d10016762
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 21:23:58 -0400

    specified initial routes that we want

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :accounts
-  resources :users
+
+
+
+
+  resources :categories, only: [:new, :create, :show, :index, :edit, :update]
+  resources :accounts, only: [:new, :create, :show, :index, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
