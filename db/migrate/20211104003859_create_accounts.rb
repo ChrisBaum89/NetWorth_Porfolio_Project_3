63966ca742f82aa92ade27fb4b6976b97dccfe12
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 20:42:01 -0400

    added columns

--- db/migrate/20211104003859_create_accounts.rb
+++ db/migrate/20211104003859_create_accounts.rb
@@ -1,7 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration[6.1]
   def change
     create_table :accounts do |t|
-
+      t.string :name
+      t.string :type
+      t.integer :dollar_value
       t.timestamps
     end
   end
