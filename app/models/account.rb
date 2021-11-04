2ef9694012359c9c9ca1ced2a8bcd8f765c40180
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-03 23:33:04 -0400

    added method to change value to negative if debt

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,11 @@
 class Account < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  def asset_or_debt
+    if self.account_type == "debt"
+      self.dollar_value = 0 - self.dollar_value
+    end
+    self.save
+  end
 end
