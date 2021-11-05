f25625f31c9cdc76328c2bb564559c6c46eb94b4
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-04 23:30:43 -0400

    added multiple stats

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,22 @@
 class AdminController < ApplicationController
   def home
+    @number_of_users = User.count
 
+    @total_net_worth = 0
+    User.all.each do |user|
+      @total_net_worth = @total_net_worth + user.net_worth
+    end
+
+    @average_net_worth = @total_net_worth / @number_of_users
+
+    @total_debt = 0
+    Account.all.each do |account|
+      if account.account_type == "Debt"
+        @total_debt = @total_debt + account.dollar_value
+      end
+    end
+
+    @average_debt = @total_debt / @number_of_users
   end
 
 end
