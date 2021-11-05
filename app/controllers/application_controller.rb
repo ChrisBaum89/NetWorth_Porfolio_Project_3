9c6d05dfb6bede1a30eb6fd4efd81971ea8ed1fc
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-04 21:25:18 -0400

    added current_user and logged_in? methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 
   def current_user
+    @user = User.find_by_id(session[:user_id])
+  end
 
+  def logged_in?
+    current_user != nil
   end
 end
