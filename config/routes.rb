c30156ca4a72ec21ea592888ebcfe6e60c58d968
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-07 13:31:15 -0500

    added users index page (for admins)

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   resources :categories, only: [:new, :create, :show, :index, :edit, :update]
   resources :accounts, only: [:new, :create, :show, :index, :edit, :update]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
