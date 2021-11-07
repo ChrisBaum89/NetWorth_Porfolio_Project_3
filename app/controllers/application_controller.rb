5a26d57dda035253208462bd904e2bea4f42ffdb
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-07 13:47:34 -0500

    changed to not setting the class variable to avoid reassignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?, :current_user, :admin?
 
   def current_user
-    @user = User.find_by_id(session[:user_id])
+    User.find_by_id(session[:user_id])
   end
 
   def logged_in?
