4856bdf6a211d7941f33a02f42d588a7aced961e
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-07 15:42:44 -0500

    added validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,10 @@ class Account < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  validates :name, presence: true
+  validates :account_type, presence: true
+  validates :dollar_value, presence: true
+
   def asset_or_debt
     if self.account_type == "debt" && self.dollar_value > 0
       self.dollar_value = 0 - self.dollar_value
