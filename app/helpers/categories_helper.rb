acec3bc71006c9d4d00da631dfcb36fac5e14485
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-07 12:52:28 -0500

    added users_count and total_value methods

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,14 @@
 module CategoriesHelper
+
+  def users_count(category)
+    category.users.uniq.count
+  end
+
+  def total_value(category)
+    total_value = 0
+    category.accounts.uniq.each do |account|
+      total_value = total_value + account.dollar_value
+    end
+    total_value
+  end
 end
