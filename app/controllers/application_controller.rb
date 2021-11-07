c782224d491c8943087d220d2e586e5d1317ee7b
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-07 12:18:45 -0500

    added admin? to helper method list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?, :current_user
+  helper_method :logged_in?, :current_user, :admin?
 
   def current_user
     @user = User.find_by_id(session[:user_id])
