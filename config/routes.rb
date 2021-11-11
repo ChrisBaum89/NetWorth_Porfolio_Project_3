fd7818e4e7a155b0b00c9da8015892ee594f15d9
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-10 19:12:05 -0500

    updated routes to have a nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
 
   get '/admin/home' => 'admin#home'
 
-  resources :categories, only: [:new, :create, :show, :index, :edit, :update, :destroy]
+  resources :categories, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  resources :categories, only: [:index] do
+    resources :users, only:[:index]
+  end
+
   resources :accounts, only: [:new, :create, :show, :index, :edit, :update, :destroy]
   resources :users, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
