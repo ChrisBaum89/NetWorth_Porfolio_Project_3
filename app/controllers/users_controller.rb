73daaded2241cb26f56aa1adc46d534d1448ac96
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-10 20:37:21 -0500

    changed redirect to render :new to display error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
-        redirect_to new_user_path
+        render :new
       end
     else
       redirect_to new_user_path
