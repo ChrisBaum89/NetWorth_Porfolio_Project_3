147dd6e3789f4eec4b590ad1dfda587a99be7e63
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-11 19:02:10 -0500

    updated with uid column on users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_170213) do
+ActiveRecord::Schema.define(version: 2021_11_11_235718) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_170213) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.integer "uid", default: 0
   end
 
 end
