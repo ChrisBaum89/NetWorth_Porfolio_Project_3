20921e3fb20748738df7439f8720a6e1c8411b50
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-11 23:06:41 -0500

    moved many methods to helper method file

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,38 +4,6 @@ class AdminController < ApplicationController
   #home route
   def home
     @number_of_users = User.count
-    all_users_net_worth
-    all_users_average_net_worth
-    all_users_debt
-    all_users_average_debt
-  end
-
-  #calculate net worth for all users
-  def all_users_net_worth
-    @total_net_worth = 0
-    User.all.each do |user|
-      @total_net_worth = @total_net_worth + user.net_worth
-    end
-  end
-
-  #calculate average net worth for all users
-  def all_users_average_net_worth
-    @average_net_worth = @total_net_worth / @number_of_users
-  end
-
-  #calculate debt for all users
-  def all_users_debt
-    @total_debt = 0
-    Account.all.each do |account|
-      if account.account_type == "Debt"
-        @total_debt = @total_debt + account.dollar_value
-      end
-    end
-  end
-
-  #calculate average debt for all users
-  def all_users_average_debt
-    @average_debt = @total_debt / @number_of_users
   end
 
 end
