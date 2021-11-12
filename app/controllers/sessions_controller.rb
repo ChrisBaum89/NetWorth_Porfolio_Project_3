ecb2dc7bcf8dbe1825186a19c677ac3cebadd654
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-11 20:50:08 -0500

    restructured create logic for correct redirecting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,40 +1,52 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new
+
+    #if session already created, always redirect back to user show
     if session[:id]
       redirect_to user_path(@user)
-    elsif params[:username] != nil && params[:password]
+
+    elsif params[:username] != nil && params[:password] != nil && auth == nil
       if @user = User.find_by(username: params[:username])
-        return head(:forbidden) unless @user.authenticate(params[:password])
+        user_found = true
+      end
+
+      if @user.authenticate(params[:password])
+        password_authenticated = true
+      end
+
+      if user_found == true && password_authenticated == true
+        session[:user_id] = @user.id
+        session[:username] = @user.username
+        if admin?
+          redirect_to admin_home_path
+        else
+          redirect_to user_path(@user)
+        end
       else
         redirect_to signin_path
       end
-      session[:user_id] = @user.id
-      session[:username] = @user.username
-      if admin?
-        redirect_to admin_home_path
-      else
-        redirect_to user_path(@user)
-      end
-    elsif auth['uid']
+
+    #login using Omniauth (facebook)
+    elsif auth
       if @user = User.find_by(uid: auth[:uid].to_i)
         @user.username = auth[:info][:name]
         @user.email = auth[:info][:email]
+        redirect_to user_path(@user)
       else
-        @user = User.new
-        @user.username = auth[:info][:name]
-        @user.email = auth[:info][:email]
-        @user.uid = auth[:uid]
-        @user.password = SecureRandom.urlsafe_base64
-        @user.password_confirmation = @user.password
-        @user.save
+        omniauth_new_user
+        if @user.save
+          redirect_to user_path(@user)
+        else
+          render :new
+        end
       end
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
     else
-      redirect_to signin_path
     end
   end
 
@@ -47,6 +59,16 @@ class SessionsController < ApplicationController
 
   def auth
     request.env['omniauth.auth']
+  end
+
+  def omniauth_new_user
+    @user = User.new
+    @user.username = auth[:info][:name]
+    @user.email = auth[:info][:email]
+    @user.uid = auth[:uid]
+    #assign random password for omniauth user
+    @user.password = SecureRandom.urlsafe_base64
+    @user.password_confirmation = @user.password
   end
 
 end
