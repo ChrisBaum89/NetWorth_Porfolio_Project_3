ffc78c42be3e5566a64aa941aa384b515457c718
Chris Baum <Christopher.Baum@valpo.edu>
2021-11-11 19:33:14 -0500

    modified so net worth had a default value of 0

--- db/migrate/20211104003836_create_users.rb
+++ db/migrate/20211104003836_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :username
       t.string :email
       t.string :password_digest
-      t.integer :net_worth
+      t.integer :net_worth, default: 0
       t.timestamps
     end
   end
